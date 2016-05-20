80771e531bcb79a2e7b19649a4860c60dd8c865b
jccultima123 <johncyrillcorsanes@gmail.com>
2016-05-20 17:31:49 +0800

    [FIX] Several adjustments

--- app/www/js/app.js
+++ app/www/js/app.js
@@ -8,7 +8,7 @@
 
 /*global angular*/
 
-angular.module('starter', ['ionic', 'ionic-material', 'starter.controllers', 'starter.services', 'ngRiffle'])
+angular.module('starter', ['ionic', 'ionic-material', 'starter.controllers', 'starter.services'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -17,7 +17,6 @@ angular.module('starter', ['ionic', 'ionic-material', 'starter.controllers', 'st
     if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
       cordova.plugins.Keyboard.disableScroll(true);
-
     }
     if (window.StatusBar) {
       // org.apache.cordova.statusbar required
@@ -26,60 +25,101 @@ angular.module('starter', ['ionic', 'ionic-material', 'starter.controllers', 'st
   });
 })
 
-.config(function($riffleProvider){
-    $riffleProvider.setDomain("REPLACEME");
-})
-
 .config(function($stateProvider, $urlRouterProvider) {
 
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
   $stateProvider
-    .state('eventmenu', {
-      url: '/event',
-      abstract: true,
-      templateUrl: 'templates/event-menu.html'
-    })
-    .state('eventmenu.home', {
-      url: '/home',
-      views: {
-        'menuContent' :{
-          templateUrl: 'templates/home.html'
-        }
+  // setup an abstract state for the tabs directive
+  .state('tab', {
+    url: '/tab',
+    abstract: true,
+    templateUrl: 'templates/tabs.html'
+  })
+  // Each tab has its own nav history stack:
+  .state('tab.locate', {
+    url: '/locate',
+    views: {
+      'tab-locate': {
+        templateUrl: 'templates/tab-locate.html',
+        // controller: 'heatmapController'
       }
-    })
-    .state('eventmenu.checkin', {
-      url: '/check-in',
-      views: {
-        'menuContent' :{
-          templateUrl: 'templates/check-in.html',
-          controller: 'CheckinCtrl'
-        }
+    }
+  })
+  .state('tab.tips', {
+    url: '/tips',
+    views: {
+      'tab-tips': {
+        templateUrl: 'templates/tab-tips.html'
       }
-    })
-    .state('eventmenu.attendees', {
-      url: '/attendees',
-      views: {
-        'menuContent' :{
-          templateUrl: 'templates/attendees.html',
-          controller: 'AttendeesCtrl'
-        }
+    }
+  })
+  .state('tab.feed', {
+    url: '/feed',
+    views: {
+      'tab-feed': {
+        templateUrl: 'templates/tab-feed.html'
       }
-    })
-    .state('tab', {
-      url: '/tab',
-      abstract: true,
-      templateUrl: 'templates/tabs.html'
-    })
-    .state('tab.dash', {
-      url: '/dash',
-      views: {
-        'tab-dash': {
-          templateUrl: 'templates/tab-dash.html',
-          controller: 'DashCtrl'
-        }
+    }
+  })
+  .state('tab.law', {
+    url: '/law',
+    views: {
+      'tab-law': {
+        templateUrl: 'templates/tab-law.html',
+        controller: 'ChatsCtrl'
       }
-    });
+    }
+  })
+  .state('tab.chat-detail', {
+    url: '/law/:chatId',
+    views: {
+      'tab-law': {
+        templateUrl: 'templates/chat-detail.html',
+        controller: 'ChatDetailCtrl'
+      }
+    }
+  })
+  .state('tab.menu', {
+    url: '/menu',
+    views: {
+      'tab-menu': {
+        templateUrl: 'templates/tab-menu.html',
+        controller: 'DashCtrl'
+      }
+    }
+  })
+  .state('tab.login', {
+    url: '/menu/login',
+    views: {
+      'tab-menu': {
+        templateUrl: 'templates/tab-login.html',
+        controller: 'DashCtrl'
+      }
+    }
+  })
+  .state('tab.signup', {
+    url: '/menu/signup',
+    views: {
+      'tab-menu': {
+        templateUrl: 'templates/tab-signup.html',
+        controller: 'DashCtrl'
+      }
+    }
+  })
+  .state('tab.others', {
+    url: '/menu/others',
+    views: {
+      'tab-menu': {
+        templateUrl: 'templates/tab-others.html',
+        controller: 'DashCtrl'
+      }
+    }
+  })
+  ;
 
-  // $urlRouterProvider.otherwise('/tab/dash');
-  $urlRouterProvider.otherwise('/event/home');
-
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/tab/feed');
 });
--- app/www/js/services.js
+++ app/www/js/services.js
@@ -4,29 +4,35 @@ angular.module('starter.services', [])
   // Might use a resource here that returns a JSON array
 
   // Some fake testing data
+  var login = {
+    id:0,
+    name: 'John Anthony',
+    email: 'anthony@fila.com',
+    password: '123'
+  };
   var chats = [{
     id: 0,
-    name: 'Ben Sparrow',
+    name: 'Sample Law 1',
     lastText: 'You on your way?',
     face: 'img/ben.png'
   }, {
     id: 1,
-    name: 'Max Lynx',
+    name: 'Sample Law 2',
     lastText: 'Hey, it\'s me',
     face: 'img/max.png'
   }, {
     id: 2,
-    name: 'Adam Bradleyson',
+    name: 'Sample Law 3',
     lastText: 'I should buy a boat',
     face: 'img/adam.jpg'
   }, {
     id: 3,
-    name: 'Perry Governor',
+    name: 'Sample Law 4',
     lastText: 'Look at my mukluks!',
     face: 'img/perry.png'
   }, {
     id: 4,
-    name: 'Mike Harrington',
+    name: 'Sample Law 5',
     lastText: 'This is wicked good ice cream.',
     face: 'img/mike.png'
   }];
