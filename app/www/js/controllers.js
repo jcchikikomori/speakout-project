b7f5b70d021272d62c97bea6d1c681f7e215fa98
jccultima123 <johncyrillcorsanes@gmail.com>
2016-05-21 19:02:50 +0800

    [TEST] New Leaflet setup with geolocation

--- app/www/js/controllers.js
+++ app/www/js/controllers.js
@@ -114,10 +114,6 @@ angular.module('starter.controllers', [])
     ionicMaterialInk.displayEffect();
 })
 
-.controller('HeatMapCtrl', function($scope, $stateParams, $timeout) {
-    $scope.$parent.hideHeader(); //workaround
-})
-
 .controller('ActivityCtrl', function($scope, $stateParams, $timeout, ionicMaterialMotion, ionicMaterialInk) {
     $scope.$parent.showHeader();
     $scope.$parent.clearFabs();
@@ -153,5 +149,148 @@ angular.module('starter.controllers', [])
     });
 
 })
+
+.controller('HeatMapCtrl',
+  [ '$scope',
+    '$cordovaGeolocation',
+    '$stateParams',
+    '$ionicModal',
+    '$ionicPopup',
+    'LocationsService',
+    'InstructionsService',
+    function(
+      $scope,
+      $cordovaGeolocation,
+      $stateParams,
+      $ionicModal,
+      $ionicPopup,
+      LocationsService,
+      InstructionsService
+      ) {
+
+      /**
+       * Once state loaded, get put map on scope.
+       */
+      $scope.$on("$stateChangeSuccess", function() {
+
+        $scope.locations = LocationsService.savedLocations;
+        $scope.newLocation;
+
+        if(!InstructionsService.instructions.newLocations.seen) {
+          var instructionsPopup = $ionicPopup.alert({
+            title: 'Add Locations',
+            template: InstructionsService.instructions.newLocations.text
+          });
+          instructionsPopup.then(function(res) {
+            InstructionsService.instructions.newLocations.seen = true;
+          });
+        }
+
+        $scope.map = {
+          center: {
+            lat: 14.6898813666188,
+            lng: 120.9921634,
+            zoom: 12
+          },
+          defaults: {
+            tileLayer: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
+            maxZoom: 18,
+            zoomControlPosition: 'bottomleft',
+            scrollWheelZoom: false
+          },
+          markers : {},
+          events: {
+            map: {
+              enable: ['context'],
+              logic: 'emit'
+            }
+          }
+        };
+        $scope.goTo(0);
+      });
+
+      var Location = function() {
+        if ( !(this instanceof Location) ) return new Location();
+        this.lat  = "";
+        this.lng  = "";
+        this.name = "";
+      };
+
+      $ionicModal.fromTemplateUrl('templates/addLocation.html', {
+        scope: $scope,
+        animation: 'slide-in-up'
+      }).then(function(modal) {
+          $scope.modal = modal;
+      });
+
+      /**
+       * Detect user long-pressing on map to add new location
+       */
+      $scope.$on('leafletDirectiveMap.contextmenu', function(event, locationEvent){
+        $scope.newLocation = new Location();
+        $scope.newLocation.lat = locationEvent.leafletEvent.latlng.lat;
+        $scope.newLocation.lng = locationEvent.leafletEvent.latlng.lng;
+        $scope.modal.show();
+      });
+
+      $scope.saveLocation = function() {
+        LocationsService.savedLocations.push($scope.newLocation);
+        $scope.modal.hide();
+        $scope.goTo(LocationsService.savedLocations.length - 1);
+      };
+
+      /**
+       * Center map on specific saved location
+       * @param locationKey
+       */
+      $scope.goTo = function(locationKey) {
+
+        var location = LocationsService.savedLocations[locationKey];
+
+        $scope.map.center  = {
+          lat : location.lat,
+          lng : location.lng,
+          zoom : 12
+        };
+
+        $scope.map.markers[locationKey] = {
+          lat:location.lat,
+          lng:location.lng,
+          message: location.name,
+          focus: true,
+          draggable: false
+        };
+
+      };
+
+      /**
+       * Center map on user's current position
+       */
+      $scope.locate = function(){
+
+        $cordovaGeolocation
+          .getCurrentPosition()
+          .then(function (position) {
+            $scope.map.center.lat  = position.coords.latitude;
+            $scope.map.center.lng = position.coords.longitude;
+            $scope.map.center.zoom = 15;
+
+            $scope.map.markers.now = {
+              lat:position.coords.latitude,
+              lng:position.coords.longitude,
+              message: "You Are Here",
+              focus: true,
+              draggable: false
+            };
+
+          }, function(err) {
+            // error
+            console.log("Location error!");
+            console.log(err);
+          });
+
+      };
+
+}])
 
 ;
